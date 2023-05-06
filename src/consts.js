bd5413f39db884e8ba4345922b6b2275821d4897
Zidanalfdllh <116675088+19221225@users.noreply.github.com>
2023-05-06 18:40:54 +0700

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Z Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `Blog Test`;
+export const SITE_DESCRIPTION = 'Blog';
 export const SITE_EMAIL = '19221125@bsi.ac.id'
 export const SITE_NAME = 'https://astro-air-test.vercel.app';
 export const SITE_URL = "https://astro-air-test.vercel.app";
