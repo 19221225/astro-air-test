42441f0ae98f6499b41ad65e85362063d9bc7f7f
Zidanalfdllh <116675088+19221225@users.noreply.github.com>
2023-06-08 12:47:36 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ export function formatDate(dateString) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return year + " Tahun " + month + " Bulan " + day + " Hari";
+  return year + "  " + month + "  " + day + " ";
 }
 
 // debounce function
