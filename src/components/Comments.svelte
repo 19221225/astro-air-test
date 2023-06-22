0b2c24cb20b32a0c7009145b0a3928ac4a141241
Zidanalfdllh <116675088+19221225@users.noreply.github.com>
2023-06-22 16:04:39 +0700

    Add files via upload

---
+++ src/components/Comments.svelte
@@ -0,0 +1,16 @@
+<script>
+import {onMount} from "svelte"
+onMount (() => {
+    const s = document.createElement('script')
+    const tag = document.getElementById('utterances')
+    s.setAttribute('repo', '19221225/astro-air-test')
+    s.setAttribute('issue-term', 'pathname')
+    s.setAttribute('label', 'comments')
+    s.setAttribute('theme', 'github=dark')
+    s.setAttribute('crossorigin','anonymous')
+    s.src = 'https://utteranc.es/client.js'
+    tag.parentNode.insertBefore(s,tag)
+})
+</script>
+
+<div id="utterances"></div>
